45aca807c591537183971881deae9b9d8c469119
Stephen Li <stephen.li305@gmail.com>
2019-08-08 14:34:15 -0700

    Updates

---
+++ precache-manifest.8656eef264030dba3b6133912705a5d2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0738fc8377680c3755ca5769a2675d",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "c39a02612df5a7da3e69",
+    "url": "/WeatherApp/static/css/main.fcab5801.chunk.css"
+  },
+  {
+    "revision": "e9f9727c6534de6d878a",
+    "url": "/WeatherApp/static/js/2.9ebd63bc.chunk.js"
+  },
+  {
+    "revision": "c39a02612df5a7da3e69",
+    "url": "/WeatherApp/static/js/main.18af4a2d.chunk.js"
+  },
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  },
+  {
+    "revision": "5e3297c84cac8f8756128ccc83d3a00e",
+    "url": "/WeatherApp/static/media/ClearSkies.5e3297c8.jpg"
+  },
+  {
+    "revision": "4545569667ed19b27767a7785ffa09d4",
+    "url": "/WeatherApp/static/media/FewClouds.45455696.jpg"
+  },
+  {
+    "revision": "04414fa64d7a156f24dabc4e6c0729f4",
+    "url": "/WeatherApp/static/media/Mist.04414fa6.jpg"
+  },
+  {
+    "revision": "ddb310e8261b37730703f828435683b3",
+    "url": "/WeatherApp/static/media/Rain.ddb310e8.gif"
+  },
+  {
+    "revision": "6605f1df45a2249e81eb480c4931460e",
+    "url": "/WeatherApp/static/media/ScatterClouds.6605f1df.jpg"
+  },
+  {
+    "revision": "32f18b14a572764284ef84f81752aed1",
+    "url": "/WeatherApp/static/media/Snow.32f18b14.jpg"
+  },
+  {
+    "revision": "a71db71c0da1836e9c517c7d0e19123a",
+    "url": "/WeatherApp/static/media/Thunderstorm.a71db71c.jpg"
+  },
+  {
+    "revision": "571809a12cbc477a8297d2898b561770",
+    "url": "/WeatherApp/static/media/brokenClouds.571809a1.png"
+  },
+  {
+    "revision": "eeaf297b821bb8cb3f1a6a65b2caa2e5",
+    "url": "/WeatherApp/static/media/showerRain.eeaf297b.jpg"
+  }
+]);
