393bab0164587dad6ba8ab11b580c12ee761df99
Stephen Li <stephen.li305@gmail.com>
2019-08-13 09:55:09 -0700

    finished styleing weather component

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -5,21 +5,44 @@ const Weather = props => {
     return(
         <div className="weather__info">
             {
-                props.city && props.country && <p className="weather__key">Location: 
-                    <span>{props.city}, {props.country}</span>
+                props.city && props.country && <p className="weather__key">Location:
+                    <span className="weather__value"> 
+                         {props.city}, {props.country
+                    }</span>
                 </p>
             }
 
             {
-                props.farenheit && <p>Farenheit: 
-                    <span>
+                props.farenheit && <p className="weather__key">Farenheit: 
+                    <span className="weather__value">
                         {props.farenheit}°F
                     </span>
                 </p>
             }
-            {props.humidity && <p>Humidity: {props.humidity}%</p>}
-            {props.description && <p>Description: {props.description}</p>}
-            {props.error && <p>Error: {props.error}</p>}
+
+            {
+                props.humidity && <p className="weather__key">Humidity: 
+                    <span className="weather__value">
+                        {props.humidity}% 
+                    </span>
+                </p>
+            }
+
+            {
+                props.description && <p className="weather__key">Description: 
+                    <span className="weather__value">
+                        {props.description}
+                    </span>
+                </p>
+            }
+            
+            {
+                props.error && <p className="weather__key">Error: 
+                    <span className="weather__value">
+                        {props.error}
+                    </span>
+                </p>
+            }
         </div>
     )
 }
